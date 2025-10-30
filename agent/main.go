c0ca3c097f4fabc7b652ad6a290b25d7b896540a
Lukluk santoso <luklukaha@gmail.com>
2025-10-30 10:36:03 +0700

    add proto

---
+++ agent/main.go
@@ -0,0 +1,11 @@
+//@TODO: Implement NodeAgent
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
---
+++ centro/main.go
@@ -0,0 +1,11 @@
+//@TODO: Implement Centro
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
